ed55a9ef3f2e4e510cbcd68e86c9abcf960ccfdd
LoiBui489 <88994645+LoiBui489@users.noreply.github.com>
2022-08-22 21:44:45 +0700

    Add project files.

---
+++ QLBH.DAL/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLBH.DAL.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            OrdersDetails = new HashSet<OrdersDetail>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Supplier { get; set; }
+        public string Descriptions { get; set; }
+        public decimal? Price { get; set; }
+        public string Images { get; set; }
+        public bool? OnSale { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ICollection<OrdersDetail> OrdersDetails { get; set; }
+    }
+}
---
+++ QLBH.DAL/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace QLBH.DAL.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string UserAddress { get; set; }
+        public string Username { get; set; }
+        public string Pwd { get; set; }
+        public string UserRole { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
